9dd7d7ba96d818edfddfd263b55211be73f28598
Landry Norris <landry.norris@live.com>
2023-02-13 19:18:46 -0600

    Fix UI for game over popup

--- shared/src/composeMain/kotlin/com/memorizepi/ui/GameOverPopup.kt
+++ shared/src/composeMain/kotlin/com/memorizepi/ui/GameOverPopup.kt
@@ -1,18 +1,25 @@
 package com.memorizepi.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 internal fun GameOverPopup(returnToMenu: () -> Unit = {}, retry: () -> Unit = {}) {
     Popup(onDismissRequest = {}, popupPositionProvider = positionProvider()) {
-        Column {
-            Text("Game Over")
-
+        Column(Modifier.background(MaterialTheme.colors.surface).size(150.dp, 100.dp),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Spacer(modifier = Modifier.weight(1f))
+            Text("Game Over", color = MaterialTheme.colors.onSurface)
+            Spacer(modifier = Modifier.weight(1f))
             Row(horizontalArrangement = Arrangement.SpaceEvenly) {
                 Button(content = { Text("Menu") },
                     onClick = returnToMenu)
